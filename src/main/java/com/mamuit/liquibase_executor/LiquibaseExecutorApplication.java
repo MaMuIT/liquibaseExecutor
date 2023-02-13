3e570e984270c5acf048c070f7c7ad69b2a13f6a
MaMuIT <mad-max85@web.de>
2023-02-13 18:49:06 +0100

    init

---
+++ src/main/java/com/mamuit/liquibase_executor/LiquibaseExecutorApplication.java
@@ -0,0 +1,13 @@
+package com.mamuit.liquibase_executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseExecutorApplication.class, args);
+    }
+
+}
